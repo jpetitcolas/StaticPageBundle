c1ff6ac4478c48488020f68da096a71e269bdb15
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2012-03-06 08:57:21 +0100

    Create static page controller

--- Controller/StaticPageController.php
+++ Controller/StaticPageController.php
@@ -6,14 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 class StaticPageController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * Render a static page by its name.
+     * @param string $page Template (and page) name
+     * @return Response
+     *
+     * @throws NotFoundHttpException If the template does not exist.
+     *
+     * @Route("/{page}")
      */
-    public function indexAction($name)
+    public function indexAction($page = 'home')
     {
-        return array('name' => $name);
+        // Do not deliver home page at "/home" URL (avoid duplicate content)
+        $requestUri = $this->getRequest()->getRequestUri();
+        if(strpos($requestUri, '/home')) {
+            return $this->redirect('/', 301);
+        }
+
+        $template = sprintf('JPetitcolasStaticPageBundle:StaticPage:%s.html.twig', $page);
+        if(!$this->get('templating')->exists($template)) {
+            throw new NotFoundHttpException(sprintf('Unable to find template %s.', $template));
+        }
+
+        return $this->render($template);
     }
 }
